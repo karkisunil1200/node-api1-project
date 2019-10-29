a9e7b6f258ace059c04de1f2fea926dafffb25a3
karkisunil1200 <karkisunil1200@gmail.com>
2019-10-29 07:59:41 -0500

    Created get request

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Implement your API here
+
+// import express
+const express = require('express');
+
+const db = '.data/db.js'; // creates the database
+
+const server = express(); // creates a server
+
+//GET the users
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      console.log('error', err);
+      res.json({ error: 'failed to get users from db' });
+    });
+});
