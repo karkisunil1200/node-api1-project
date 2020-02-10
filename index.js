22a5c053472009c2374397d8a152e62df24169fa
Sunil Karki <karkisunil1200@gmail.com>
2020-02-10 16:55:34 -0600

    Added express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+const express = require('express');
+
+const Hubs = require('./data/db');
+
+const server = express();
+
+const PORT = 5000;
+server.listen('PORT', () => console.log('Localhost5000 is up and running'));
